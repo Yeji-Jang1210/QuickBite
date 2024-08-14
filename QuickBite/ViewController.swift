8be936c586ffc83dc82cf6de00b3a12b50065e31
Yeji-Jang1210 <jyg8727@gmail.com>
2024-08-14 13:57:39 +0900

    Initial Commit

---
+++ QuickBite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickBite
+//
+//  Created by 장예지 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
